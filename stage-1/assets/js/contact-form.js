57d5b4a109a2951f518c4e597cbf5a7d44e89fdc
gulpanjul <gulpa.andhikac@gmail.com>
2025-02-11 01:48:46 +0700

    feat: async, promises and ajax

--- stage-1/assets/js/contact-form.js
+++ stage-1/assets/js/contact-form.js
@@ -1,4 +1,4 @@
-function submitForm() {
+function submitForm(event) {
     event.preventDefault();
     let name = document.getElementById("name").value;
     let email = document.getElementById("email").value;
@@ -8,11 +8,9 @@ function submitForm() {
 
     //alert(`Name: ${name}\nEmail: ${email}\nSubject: ${subject}\nskill: ${skill}\nMessage: ${message}`);
 
-    let emailTujuan = "D5jg3@example.com";
+    let emailTujuan = 'gulpa.andhikac@gmail.com';
 
-    let a = document.createElement("a");
-
-    //a.href = `mailto:${emailTujuan}?subject=Contact Form&body=Name: ${name}\nEmail: ${email}\nSubject: ${subject}\nskill: ${skill}.\nMessage: ${message}`;
+    let a = document.createElement('a');
 
     a.href = `mailto:${emailTujuan}?subject=${subject}&body=${`Halo, nama saya ${name}. Silahkan hubungi saya di ${email}. Skill saya adalah ${skill}. Berikut yang ingin saya sampaikan: ${message}`}`;
 
